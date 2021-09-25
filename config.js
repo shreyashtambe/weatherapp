0f1eb38af211150be51891e70f7393ecf39e576c
shreyash tambe <shreyashtambe7171@gmail.com>
2021-09-25 12:42:19 +0530

    initial commit

---
+++ config.js
@@ -0,0 +1,10 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "bb3553087ba76d551393b3801ef246f6"
+    }
+}
+
+module.exports = constants;
+
+//   https://api.openweathermap.org/data/2.5/weather?q=Lahore&appid=bb3553087ba76d551393b3801ef246f6
